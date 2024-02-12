c8c22021fa88392053b471b6ddaea467ea890fb9
unknown <yusquen22@gmail.com>
2024-02-12 18:37:21 -0500

    Here I added a Card component and then imported it in Pages/Home. My first card on our E-commerce web app :=)

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -68,7 +68,7 @@ const Navbar = () => {
       </ul>
       <ul className='flex items-center gap-3'>
         <li className='text-black/60'>
-          teff@platzi.com
+          yusquen22@gmail.com
         </li>
         <li>
           <NavLink
