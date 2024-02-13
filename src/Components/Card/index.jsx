c9392877d6baa7e220fb1f4675416be3e7b5d0cd
unknown <yusquen22@gmail.com>
2024-02-13 18:09:07 -0500

    Adding a new Component: ProductDetail... in the /Context I used the Hook useState to create a state productToShow... then useContext to showProduct

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -5,8 +5,15 @@ import { ShoppingCartContext } from '../../Context'
 const Card = (data) => {
   const context = useContext(ShoppingCartContext)
 
+  const showProduct = (productDetail) => {
+    context.openProductDetail()
+    context.setProductToShow(productDetail)
+  }
+
   return (
-    <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
+    <div
+      className='bg-white cursor-pointer w-56 h-60 rounded-lg'
+      onClick={() => showProduct(data.data)}>
       <figure className='relative mb-2 w-full h-4/5'>
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category.name}</span>
         <img className='w-full h-full object-cover rounded-lg' src={data.data.image} alt={data.data.title} />
@@ -15,7 +22,7 @@ const Card = (data) => {
           onClick={() => context.setCount(context.count + 1)}>
           <PlusIcon className='h-6 w-6 text-black'></PlusIcon>
         </div>
-      </figure>                             
+      </figure>
       <p className='flex justify-between'>
         <span className='text-sm font-light truncate mr-2'>{data.data.title}</span>
         <span className='text-lg font-medium'>${data.data.price}</span>
