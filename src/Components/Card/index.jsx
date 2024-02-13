6cec0b1b15186b270ad994b619de21d4a3011125
unknown <yusquen22@gmail.com>
2024-02-13 11:52:07 -0500

    Creating new component called: ProductDetail, inside import { XMarkIcon } from '@heroicons/react/24/solid' . Then I used heroicons in Navbar and Card Components

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { PlusIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 
 const Card = (data) => {
@@ -8,15 +9,15 @@ const Card = (data) => {
     <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
       <figure className='relative mb-2 w-full h-4/5'>
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category.name}</span>
-        <img className='w-full h-full object-cover rounded-lg' src={data.data.images[0]} alt={data.data.title} />
+        <img className='w-full h-full object-cover rounded-lg' src={data.data.image} alt={data.data.title} />
         <div
           className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
           onClick={() => context.setCount(context.count + 1)}>
-          +
+          <PlusIcon className='h-6 w-6 text-black'></PlusIcon>
         </div>
-      </figure>
+      </figure>                             
       <p className='flex justify-between'>
-        <span className='text-sm font-light'>{data.data.title}</span>
+        <span className='text-sm font-light truncate mr-2'>{data.data.title}</span>
         <span className='text-lg font-medium'>${data.data.price}</span>
       </p>
     </div>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,16 @@
 import { useState, useEffect } from 'react'
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
+import ProductDetail from '../../Components/ProductDetail'
 
 function Home() {
   const [items, setItems] = useState(null)
 
   useEffect(() => {
-    fetch('https://api.escuelajs.co/api/v1/products')
+    
+    fetch('https://fakestoreapi.com/products')
+    //fetch('https://api.escuelajs.co/api/v1/products')
+
       .then(response => response.json())
       .then(data => setItems(data))
   }, [])
@@ -21,6 +25,7 @@ function Home() {
           ))
         }
       </div>
+      <ProductDetail />
     </Layout>
   )
 }
