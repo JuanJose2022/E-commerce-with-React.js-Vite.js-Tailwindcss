253076576c55a47c663d786d446254d68c4cadfa
unknown <yusquen22@gmail.com>
2024-02-13 19:09:02 -0500

    Adding products to cart in \Context I added: const [cartProducts, setCartProducts] = useState([]) and in \Card I added: const addProductsToCart = (productData) => {...}

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -10,6 +10,12 @@ const Card = (data) => {
     context.setProductToShow(productDetail)
   }
 
+  const addProductsToCart = (productData) => {
+    context.setCount(context.count + 1)
+    context.setCartProducts([...context.cartProducts, productData])
+    console.log('CART: ', context.cartProducts)
+  }
+
   return (
     <div
       className='bg-white cursor-pointer w-56 h-60 rounded-lg'
@@ -19,12 +25,12 @@ const Card = (data) => {
         <img className='w-full h-full object-cover rounded-lg' src={data.data.image} alt={data.data.title} />
         <div
           className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
-          onClick={() => context.setCount(context.count + 1)}>
+          onClick={() => addProductsToCart(data.data)}>
           <PlusIcon className='h-6 w-6 text-black'></PlusIcon>
         </div>
       </figure>
       <p className='flex justify-between'>
-        <span className='text-sm font-light truncate mr-2'>{data.data.title}</span>
+        <span className='text-sm font-light'>{data.data.title}</span>
         <span className='text-lg font-medium'>${data.data.price}</span>
       </p>
     </div>
